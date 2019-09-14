c8aff80b42a2f5ee93fe4867b783cf7a1d345dc5
Josh Bryson <josh@bryson.me.uk>
2019-09-14 20:48:49 +0100

    ShopPage created

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './Pages/HomePage/HomePage';
+import ShopPage from './Pages/ShopPage/ShopPage';
 
 function App() {
   return (
     <div >
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
