17fa061a4e029f5b18e1c0118c2cd6edc65b3416
Josh Bryson <josh@bryson.me.uk>
2019-09-14 21:01:49 +0100

    Header component created

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
+import Header from './Components/Header/Header';
 import HomePage from './Pages/HomePage/HomePage';
 import ShopPage from './Pages/ShopPage/ShopPage';
 
 function App() {
   return (
     <div >
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
