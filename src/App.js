f5fda1b697a6721b837829935dacd63f8aa34291
Josh Bryson <josh@bryson.me.uk>
2019-09-18 00:50:02 +0100

    Firebase auth implemented in app file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
@@ -6,18 +6,44 @@ import Header from './Components/Header/Header';
 import HomePage from './Pages/HomePage/HomePage';
 import ShopPage from './Pages/ShopPage/ShopPage';
 import AuthenticationPage from './Pages/AuthenticationPage/AuthenticationPage';
+import { auth } from './Firebase/Firebase.utils';
 
-function App() {
-  return (
-    <div >
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/shop" component={ShopPage} />
-        <Route exact path="/signin" component={AuthenticationPage} />
-      </Switch>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null 
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({
+        currentUser: user
+      })
+      console.log(user);
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div >
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/shop" component={ShopPage} />
+          <Route exact path="/signin" component={AuthenticationPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
