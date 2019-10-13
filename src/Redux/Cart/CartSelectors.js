4b6ecb645df7f3ceaa4b290fe9d70f971fb09de2
Josh Bryson <josh@bryson.me.uk>
2019-10-14 00:31:14 +0100

    Checkout page connected to Redux

--- src/Redux/Cart/CartSelectors.js
+++ src/Redux/Cart/CartSelectors.js
@@ -20,4 +20,14 @@ export const selectCartItemsCount = createSelector(
             accumulatedQuantity + cartItem.quantity, 
             0
         )
-)
+);
+
+export const selectCartTotal = createSelector(
+    [selectCartItems],
+    cartItems => 
+        cartItems.reduce(
+            (accumulatedQuantity, cartItem) => 
+            accumulatedQuantity + cartItem.quantity * cartItem.price, 
+            0
+        )
+);
