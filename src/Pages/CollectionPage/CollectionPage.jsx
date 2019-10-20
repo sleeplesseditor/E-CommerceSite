c3d2f6ef5cc57ab98bc181da24c5a9a1c11124e6
Josh Bryson <josh@bryson.me.uk>
2019-10-20 12:48:24 +0100

    CollectionPage updated and Data normalization implemented

--- src/Pages/CollectionPage/CollectionPage.jsx
+++ src/Pages/CollectionPage/CollectionPage.jsx
@@ -4,11 +4,19 @@ import { connect } from 'react-redux';
 import CollectionItem from '../../Components/CollectionItem/CollectionItem';
 import { selectCollection } from '../../Redux/Shop/ShopSelector';
 
-const CollectionPage = ({ collection }) => (
-    <div className='collection-page'>
-        <h2>Collection Page</h2>
-    </div>
-);
+const CollectionPage = ({ collection }) => {
+    const { title, items } = collection;
+    return (
+        <div className='collection-page'>
+            <h2 className='title'>{title}</h2>
+            <div className='items'>
+                {items.map(item => (
+                    <CollectionItem key={item.id} item={item} />
+                ))}
+            </div>
+        </div>
+    )
+};
 
 const mapStateToProps = (state, ownProps) => ({
     collection: selectCollection(ownProps.match.params.collectionId)(state)
