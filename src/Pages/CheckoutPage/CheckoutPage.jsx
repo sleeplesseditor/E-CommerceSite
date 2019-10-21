75089340f54b6625231e34a8afd3e3fdf057be3a
Josh Bryson <josh@bryson.me.uk>
2019-10-22 00:56:26 +0100

    Stripe payment UI implementation and Favicon updated

--- src/Pages/CheckoutPage/CheckoutPage.jsx
+++ src/Pages/CheckoutPage/CheckoutPage.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { selectCartItems, selectCartTotal } from '../../Redux/Cart/CartSelectors';
 import CheckoutItem from '../../Components/CheckoutItem/CheckoutItem';
+import StripeButton from '../../Components/StripeButton/StripeButton';
 import './CheckoutPage.scss';
 
 const CheckoutPage = ({ cartItems, total }) => (
@@ -32,6 +33,12 @@ const CheckoutPage = ({ cartItems, total }) => (
         <div className='total'>
             <span>TOTAL: ${total}</span>
         </div>
+        <div className='test-warning'>
+            *Please use the following test credit card for payments*
+            <br />
+            4242 4242 4242 4242 - Expiry Date: 01/20 – CVV: 123
+        </div>
+        <StripeButton price={total} />
     </div>
 );
 
